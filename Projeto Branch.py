2a8fced4331eb7f97040ce4e43b90ffe9e7c6c60
Cleiton Maciel <147657778+cleiton010194@users.noreply.github.com>
2023-10-23 15:53:19 -0300

    Create Projeto Branch.py

---
+++ Projeto Branch.py
@@ -0,0 +1,9 @@
+lista = []
+for recebe in range(2):
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    profissao = input('Prof: ')
+    lista.append(nome)
+    lista.append(idade)
+    lista.append(profissao)
+print(lista)
